373bb6fd24b0b4eaae2c57c4e6eb97c0f6f7f5ed
Nabilla66 <113337236+Nabilla66@users.noreply.github.com>
2022-09-14 12:22:04 +0800

    Add files via upload

---
+++ mysecondapp.py
@@ -0,0 +1,13 @@
+%%writefile mysecondapp.py
+import streamlit as st
+import pandas as pd
+
+st.header("My second Streamlit App")
+
+st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
+show = st.checkbox('I agree the terms and conditions')
+if show:
+    st.write(pd.DataFrame({
+    'Cell.size': [1, 4, 1, 1, 3, 7, 4, 3, 1, 1],
+    'Class': [0, 0, 0, 0, 1, 1, 1, 1, 0, 0]
+}))
